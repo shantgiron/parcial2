e77c3595cdb377280fd70413594f5e104ba408cd
Sarahaime Rodríguez <saraahr_@hotmail.com>
2018-07-22 21:51:11 -0400

    solicitudes amistad

--- src/main/java/modelos/Amigo.java
+++ src/main/java/modelos/Amigo.java
@@ -9,16 +9,20 @@ public class Amigo {
     @GeneratedValue
     private long id;
 
-    @OneToOne
-    private Usuario usuario;
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Usuario usuario1;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Usuario usuario2;
 
     @Column(columnDefinition =  "boolean default false")
     boolean confirmado;
 
     public Amigo(){}
 
-    public Amigo(Usuario usuario, boolean confirmado) {
-        this.usuario = usuario;
+    public Amigo(Usuario usuario1, Usuario usuario2, boolean confirmado) {
+        this.usuario1 = usuario1;
+        this.usuario2 = usuario2;
         this.confirmado = confirmado;
     }
 
@@ -30,12 +34,20 @@ public class Amigo {
         this.id = id;
     }
 
-    public Usuario getUsuario() {
-        return usuario;
+    public Usuario getUsuario1() {
+        return usuario1;
     }
 
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
+    public void setUsuario1(Usuario usuario) {
+        this.usuario1 = usuario;
+    }
+
+    public Usuario getUsuario2() {
+        return usuario2;
+    }
+
+    public void setUsuario2(Usuario usuario) {
+        this.usuario2 = usuario;
     }
 
     public boolean isConfirmado() {
